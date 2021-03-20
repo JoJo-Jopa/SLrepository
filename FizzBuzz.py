1d21d64e549e80730ebb017ec909bb6213b629f9
JoJo-Jopa <61786227+JoJo-Jopa@users.noreply.github.com>
2021-03-20 19:33:15 +0300

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(1, n):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FillBuzz")
+    elif i % 5 == 0 and i % 2 != 0:
+        print("Buzz")
+    elif i % 3 == 0 and i % 2 != 0:
+        print("Fizz")
+    elif i % 2 != 0:
+        print(i)
+# SLrepository
